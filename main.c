115d6ad937b0a5b18d8d68f64609e422d4bb4252
Timothy Redaelli <timothy@redaelli.eu>
2010-10-22 01:17:13 +0200

    Create COVERS_DIR if it's missing and refresh game list after cover downloading

--- main.c
+++ main.c
@@ -1587,6 +1587,8 @@ int main(int argc, char *argv[])
 
 			if (dialog_ret == 1) {
 				int n;
+				snprintf(filename, sizeof(filename), "/dev_hdd0/%s", COVERS_DIR);
+				mkdir(filename, S_IRWXO | S_IRWXU | S_IRWXG | S_IFDIR);
 				for (n = 0; n < max_menu_list; n++) {
 					struct stat s;
 					snprintf(filename, sizeof(filename), "/dev_hdd0/%s/%s.PNG", COVERS_DIR, menu_list[n].title_id);
@@ -1594,6 +1596,8 @@ int main(int argc, char *argv[])
 						continue;
 
 					download_cover(menu_list[n].title_id, filename);
+					// reset to update datas
+					reset_game_list(1, 0);
 				}
 			}
 		}
