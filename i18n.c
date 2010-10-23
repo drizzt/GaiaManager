de3926625c7c460c0f4cd42353ee1590bb838c79
Timothy Redaelli <timothy@redaelli.eu>
2010-10-23 15:52:04 +0200

    Indent i18n.c

--- i18n.c
+++ i18n.c
@@ -130,7 +130,8 @@ const char *const text_toinstall[] = { "to install the game?", "to install the g
 const char *const text_cover_msg[] = {
 	"Do you want to download missing covers (in /dev_hdd0/" COVERS_DIR ")?\nIt could takes awhile...\nPlease wait",
 	"Do you want to download missing covers (in /dev_hdd0/" COVERS_DIR ")?\nIt could takes awhile...\nPlease wait",
-	"Vous-voulez telecharger les jaquettes manquantes(dans /dev_hdd0/" COVERS_DIR ")?\nCela peut prendre un certain temps...\nVeuillez-Patientez",
+	"Vous-voulez telecharger les jaquettes manquantes(dans /dev_hdd0/" COVERS_DIR
+		")?\nCela peut prendre un certain temps...\nVeuillez-Patientez",
 	"Do you want to download missing covers (in /dev_hdd0/" COVERS_DIR ")?\nIt could takes awhile...\nPlease wait",
 	"Do you want to download missing covers (in /dev_hdd0/" COVERS_DIR ")?\nIt could takes awhile...\nPlease wait",
 	"Voi scaricare le cover mancanti(in /dev_hdd0/" COVERS_DIR ")?\nPotrebbe volerci un po'...\nAttendere prego",
